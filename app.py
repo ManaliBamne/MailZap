74773cce82d5f756bb6a16cb4c828d858c9dbd0c
ManaliBamne <meerabamne5@gmail.com>
2025-12-02 21:48:33 +0530

    Switch SMTP from Gmail to Mailtrap

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import traceback
 
 app = Flask(__name__)
 
-# Email configuration - we'll use environment variables
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USE_TLS'] = False
+# Email configuration - Mailtrap test SMTP
+app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'  # from Mailtrap
+app.config['MAIL_PORT'] = 587                           # OR 2525 if Mailtrap says so
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', '')
